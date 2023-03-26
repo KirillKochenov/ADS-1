a1006386859c02d3426b6224ea38d56b083b0c3e
KirillKochenov <124993480+KirillKochenov@users.noreply.github.com>
2023-03-26 23:23:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,57 +4,46 @@
 
 
 bool checkPrime(uint64_t value) {
-if (value < 2) {
-        return false;
+  if (value <= 1) {
+    return false;
+  }
+  for (int i = 2; i <= sqrt(value); i++) {
+    if (value % i == 0) {
+      return false;
     }
-    for (int i = 2; i < value; i++) {
-        if (value % i == 0) {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    uint64_t  prost = 1;
-    int num = 0;
-    while (true) {
-        bool chek = true;
-        ++prost;
-        if (prost != 2) {
-            for (int i = 2; i < prost; ++i) {
-                if (prost % i == 0)
-                    prost = false;
-            }
-        }
-        if (chek == true)
-            num++;
-        if (num == n)
-            return prost;
+  uint64_t count = 0;
+  for (int i = 0; ; i++) {
+    if (checkPrime(i) == true) {
+      count++;
     }
+    if (count == n) {
+      return i;
+    } else {
+      continue;
+    }
+}
+  return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    while (1) {
-        uint64_t k = 1;
-        value++;
-        for (uint64_t i = 2; i <=value ; i++) {
-            if (value % i == 0) {
-                k++;
-            }
-        }
-        if (k == 2) {
-            return value;
-        }
+  for (uint64_t i = value + 1; ; i++) {
+    if (checkPrime(i))
+      return i;
+  }
+}
+uint64_t sumPrime(uint64_t hbound) {
+uint64_t count = 0;
+  if (hbound > 2) {
+    for (int i = 2; i < hbound; i++) {
+      if (checkPrime(i)) {
+        count = count + i;
+      }
     }
 }
-
-uint64_t sumPrime(uint64_t hbound) {
-uint64_t  summ = 0;
-    for (uint64_t i = 2; i < hbound; i++) {
-        if (checkPrime(i)) {
-            summ += i;
-        }
-    }
-    return summ;
+return count;
 }
