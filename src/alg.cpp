90b631e13109d76b98d7d59a388b70801e2c15fb
KirillKochenov <124993480+KirillKochenov@users.noreply.github.com>
2023-03-26 23:06:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,57 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value < 2) {
+        return false;
+    }
+    for (int i = 2; i < value; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t  prost = 1;
+    int num = 0;
+    while (true) {
+        bool chek = true;
+        ++prost;
+        if (prost != 2) {
+            for (int i = 2; i < prost; ++i) {
+                if (prost % i == 0)
+                    prost = false;
+            }
+        }
+        if (chek == true)
+            num++;
+        if (num == n)
+            return prost;
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    while (1) {
+        uint64_t k = 1;
+        value++;
+        for (uint64_t i = 2; i <=value ; i++) {
+            if (value % i == 0) {
+                k++;
+            }
+        }
+        if (k == 2) {
+            return value;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t  summ = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            summ += i;
+        }
+    }
+    return summ;
 }
